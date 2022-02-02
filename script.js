a791485aac05a5611fd22d9f128c16fed921cacf
rivea0 <58330360+rivea0@users.noreply.github.com>
2022-02-02 22:42:19 +0300

    Add CaesarCipher to encrypt and decrypt text

---
+++ script.js
@@ -0,0 +1,41 @@
+class CaesarCipher {
+    constructor(newRotation) {
+        this.rotation = newRotation;
+    } 
+    
+    buildRotationMap() {
+        let rotationMap = {};
+        const length = 26;
+        const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+        for (let i = 0; i < lowercase.length; i++) {
+            let charCode = lowercase[i].charCodeAt();
+            let char = String.fromCharCode(charCode + this.rotation % length);
+            if (char < 'a' || char > 'z') {
+                let x = length;
+                char = String.fromCharCode(char.charCodeAt() - length);
+                x += 1;
+                rotationMap[lowercase[i]] = char;
+                rotationMap[lowercase[i].toUpperCase()] = char.toUpperCase();
+            } else {
+                rotationMap[lowercase[i]] = char;
+                rotationMap[lowercase[i].toUpperCase()] = char.toUpperCase();
+            }
+        }
+        return rotationMap;
+    }
+    
+    rotate(str) {
+        let cipherText = '';
+        let rotationMap = this.buildRotationMap();
+        for (let i = 0; i < str.length; i++) {
+            if (!(str[i] in rotationMap)) {
+                cipherText += str[i];
+            } else {
+                cipherText += rotationMap[str[i]];
+            }
+        }
+        return cipherText;
+    }
+}
+
+export {CaesarCipher};
