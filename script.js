609824e1143dc53972ff2630a4619aa9d3be15b5
rivea0 <58330360+rivea0@users.noreply.github.com>
2022-02-03 15:11:52 +0300

    Separate encrypt and decrypt, add getIndex

--- script.js
+++ script.js
@@ -2,37 +2,55 @@ class CaesarCipher {
     constructor(newRotation) {
         this.rotation = newRotation;
     } 
-    
-    buildRotationMap() {
-        let rotationMap = {};
-        const length = 26;
-        const lowercase = 'abcdefghijklmnopqrstuvwxyz';
-        for (let i = 0; i < lowercase.length; i++) {
-            let charCode = lowercase[i].charCodeAt();
-            let char = String.fromCharCode(charCode + this.rotation % length);
-            if (char < 'a' || char > 'z') {
-                char = String.fromCharCode(char.charCodeAt() - length);
-                rotationMap[lowercase[i]] = char;
-                rotationMap[lowercase[i].toUpperCase()] = char.toUpperCase();
-            } else {
-                rotationMap[lowercase[i]] = char;
-                rotationMap[lowercase[i].toUpperCase()] = char.toUpperCase();
-            }
-        }
-        return rotationMap;
-    }
-    
-    rotate(str) {
+
+    lowerCaseAlpha = 'abcdefghijklmnopqrstuvwxyz';
+    upperCaseAlpha = this.lowerCaseAlpha.toUpperCase();
+    alphaLength = 26;
+
+    encrypt(str) {
         let cipherText = '';
-        let rotationMap = this.buildRotationMap();
-        for (let i = 0; i < str.length; i++) {
-            if (!(str[i] in rotationMap)) {
-                cipherText += str[i];
+        for (let i = 0; i < str.length; i++)  {
+            if (this.lowerCaseAlpha.includes(str[i])) {
+                let index = this.getIndex(true, this.lowerCaseAlpha, str[i]);
+                cipherText += this.lowerCaseAlpha[index];
+            } else if (this.upperCaseAlpha.includes(str[i])) {
+                let index = this.getIndex(true, this.upperCaseAlpha, str[i]);
+                cipherText += this.upperCaseAlpha[index];
             } else {
-                cipherText += rotationMap[str[i]];
+                cipherText += str[i];
             }
         }
         return cipherText;
+    }
+
+    decrypt(str) {
+        let plainText = '';
+        for (let i = 0; i < str.length; i++) {
+            if (this.lowerCaseAlpha.includes(str[i])) {
+                let index = this.getIndex(false, this.lowerCaseAlpha, str[i]);
+                plainText += this.lowerCaseAlpha[index];
+            } else if (this.upperCaseAlpha.includes([str[i]])) {
+                let index = this.getIndex(false, this.upperCaseAlpha, str[i]);
+                plainText += this.upperCaseAlpha[index];
+            } else {
+                plainText += str[i];
+            }
+        }
+        return plainText;    
+    }
+
+    getIndex(isEncrypt, casing, char) {
+        /*
+         Modulo calculation taken from: https://stackoverflow.com/a/13163436,
+         to prevent negative results
+        */
+         if (isEncrypt) {
+            let remain = (casing.indexOf(char) + this.rotation) % this.alphaLength;
+            return Math.floor(remain >= 0 ? remain : remain + this.alphaLength);
+        } else if (!isEncrypt) {
+            let remain = (casing.indexOf(char) - this.rotation) % this.alphaLength;
+            return Math.floor(remain >= 0 ? remain : remain + this.alphaLength);
+        }
     }
 }
 
