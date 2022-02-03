3e8bb103bb9adf66d7116e9cba47d8e6c686a011
rivea0 <58330360+rivea0@users.noreply.github.com>
2022-02-03 15:13:17 +0300

    Separate encrypt and decrypt

--- events.js
+++ events.js
@@ -4,20 +4,28 @@ let encrypt = document.querySelector('#encrypt');
 let decrypt = document.querySelector('#decrypt');
 let text = document.querySelector('#text');
 let shift = document.querySelector('#rotation');
-let buttons = [encrypt, decrypt];
 
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', displayResult);
-}
-
-function displayResult() {
+encrypt.addEventListener('click', () => {
     let resultArea = document.querySelector('.result');
     if (resultArea.hasChildNodes()) {
         resultArea.removeChild(resultArea.childNodes[0]);
-    }
-    let code = new CaesarCipher(parseInt(shift.value));
-    let result = code.rotate(text.value);
+    } 
     let p = document.createElement('p');
+    let code = new CaesarCipher(parseInt(shift.value));
+    let result = code.encrypt(text.value);
+    console.log(result);
     resultArea.appendChild(p);
-    p.textContent = `Result: ${result}`;
-}
+    p.textContent = result;    
+});
+
+decrypt.addEventListener('click', () => {
+    let resultArea = document.querySelector('.result');
+    if (resultArea.hasChildNodes()) {
+        resultArea.removeChild(resultArea.childNodes[0]);
+    } 
+    let p = document.createElement('p');
+    let code = new CaesarCipher(parseInt(shift.value));
+    let result = code.decrypt(text.value); 
+    resultArea.appendChild(p);
+    p.textContent = result;        
+});
